07bad9b5522803cf50fb2098b95a83b6e1d29c0b
PS49550_DiepHuynhNhuMy <mydiep025@gmail.com>
2025-11-06 21:15:17 +0700

    update lab 1

---
+++ Lab1/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>// thư viện   
+
+int main(){ 
+    //printf("Hello woorld");// xuat ra man hinh
+    //printf("\nHello World");
+    int a,b;
+    char ten[10];// cap phat ki tu
+    printf("Nhap a va b;");
+    scanf("%d %d", &a,&b);
+    printf("Nhap vao ten toi:");
+    scanf("%s", &ten);
+    return 0;
+} 
